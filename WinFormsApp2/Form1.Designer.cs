4a57f8ce5fa747712499f0eb4d81056c2490663d
valen <valen@LAPTOP-L1VSBEBG>
2024-03-10 20:58:14 +0800

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtcop = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Решаем";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.result_click);
+            // 
+            // txtcop
+            // 
+            this.txtcop.Location = new System.Drawing.Point(95, 88);
+            this.txtcop.Name = "txtcop";
+            this.txtcop.Size = new System.Drawing.Size(100, 23);
+            this.txtcop.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кол-во копеек";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtcop);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox txtcop;
+        private Label label1;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using static WinFormsApp2.Form1;
+
+namespace WinFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+                
+            txtcop.Text = Properties.Settings.Default.cop.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_click(object sender, EventArgs e)
+        {
+            int cop;
+            try
+            {
+                cop = int.Parse(this.txtcop.Text);
+            }
+            catch(FormatException)
+            {
+                return;
+            }
+
+            Properties.Settings.Default.cop = cop;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(logic.answer(cop)); // выведем содержимое поля txtPetrovSum
+        }
+
+        public class logic
+        {
+            static String checkC(int num)
+            {
+                if (num == 1)
+                {
+                    return "копейка";
+                }
+                else if ((num % 100) > 1 && (num % 100) < 5)
+                {
+                    return "копейки";
+                }
+                else if ((num % 100) > 4 && (num % 100) < 21)
+                {
+                    return "копеек";
+                }
+                else if ((num % 100 % 10) == 1)
+                {
+                    return "копейка";
+                }
+                else if (((num % 100 % 10) > 1) && ((num % 100 % 10) < 5))
+                {
+                    return "копейки";
+                }
+                else
+                {
+                    return "копеек";
+                }
+
+
+            }
+            static String checkR(int num)
+            {
+                if ((num / 100) == 1)
+                {
+                    return "ь";
+                }
+                else if ((num / 100) > 1 && (num / 100) < 5)
+                {
+                    return "я";
+                }
+                else if ((num / 100) > 4 && (num / 100) < 21)
+                {
+                    return "ей";
+                }
+                else if ((num / 100 % 10) == 1)
+                {
+                    return "ь";
+                }
+                else if (((num / 100 % 10) > 1) && ((num / 100 % 10) < 5))
+                {
+                    return "я";
+                }
+                else
+                {
+                    return "ей";
+                }
+            }
+
+            public static String answer(int cop)
+            {
+
+
+
+                if (cop % 100 != 0 && cop / 100 != 0)
+                {
+                    return $"{cop / 100} рубл{checkR(cop)}  {cop % 100} {checkC(cop)}";
+                }
+                else if (cop / 100 != 0)
+                {
+                    return $"{cop / 100} рубл{checkR(cop)}";
+                }
+                else
+                {
+                    return $"только {cop % 100} {checkC(cop)}";
+                }
+            }
+        }
+
+    }
+}
